67a5d7dde1089d8d21837ef7f80ee95c5cfb38e7
Didier Krux <didier@krux.co>
2018-06-12 18:26:11 +0800

    move migration in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 
-const NODE_ENV = process.env.NODE_ENV;
+const NODE_ENV = process.env.NODE_ENV || 'development';
 const knexfile = require('./knexfile')[NODE_ENV]
 var knex = require('knex')(knexfile);
 
 const app = express();
 
-app.get('/', function (req, res) {
-    knex.select('*').from('users').then((data) => {
-        console.log(data);
-        res.send(data);
-        console.log('hey');
+console.log("(1/3) running [knex.migrate.latest()]");
+knex.migrate.latest().then(function () {
+    console.log("(2/3) running [knex.seed.run()]");
+    return knex.seed.run();
+}).then(function () {
+    console.log("(3/3) app ready");
+
+    app.get('/', function (req, res) {
+        knex.select('*').from('users').then((data) => {
+            console.log(data);
+            res.send(data);
+            console.log('ho');
+        });
     });
+
+    app.listen(8000);
 });
 
-app.listen(8000);
