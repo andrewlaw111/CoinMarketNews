93167224897fb587787c06e53e6819a1d0b2657e
Didier Krux <didier@krux.co>
2018-06-13 15:35:40 +0800

    fix nodemon config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import * as  express from "express";
+import * as express from "express";
 
 const NODE_ENV = process.env.NODE_ENV || "development";
 // tslint:disable-next-line:no-var-requires
@@ -7,20 +7,12 @@ const knexfile = require("../knexfile")[NODE_ENV];
 const knex = require("knex")(knexfile);
 const app = express();
 
-knex.migrate.latest().then(() => {
-    console.log("(2/3) running [knex.seed.run()]");
-    return knex.seed.run();
-}).then(function () {
-    console.log("(3/3) app ready");
-
-    app.get('/', function (req: any, res: any) {
-        knex.select("*").from("users").then((data: any) => {
-            console.log(data);
-            res.send(data);
-            console.log("ho");
-        });
+app.get('/', function (req: any, res: any) {
+    knex.select("*").from("users").then((data: any) => {
+        console.log(data);
+        res.send(data);
+        console.log("ho");
     });
-
 });
 
 app.listen(8000);
