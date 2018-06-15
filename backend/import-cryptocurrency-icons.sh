ef07001ef5cfc389a3234195269d7107fc1d1e35
Didier Krux <didier@krux.co>
2018-06-15 19:33:05 +0800

    move icons to public dir

--- backend/import-cryptocurrency-icons.sh
+++ backend/import-cryptocurrency-icons.sh
@@ -1,1 +1,1 @@
-svn export --force https://github.com/cjdowner/cryptocurrency-icons/trunk/128/color/ cryptocurrency-icons
+svn export --force https://github.com/cjdowner/cryptocurrency-icons/trunk/128/color/ public/cryptocurrency-icons
