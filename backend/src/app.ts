53cc233fa23bdd2be929318745b85e0b66b412f9
Didier Krux <didier@krux.co>
2018-06-15 20:24:12 +0800

    send coin icon + auto-generate png

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,9 @@ import CoinService from "./services/CoinService";
 import UserService from "./services/UserService";
 import { app, server } from "./utils/init-app";
 import LoginRouter from "./utils/login";
+import * as express from "express";
+const text2png = require('text2png');
+const fs = require('fs');
 
 import Cron from "./utils/cron";
 
@@ -12,6 +15,15 @@ const coinService = new CoinService();
 const userService = new UserService();
 
 Cron.price();
+
+app.use('/icon', express.static('public/cryptocurrency-icons'));
+app.use('/icon', function (req, res) {
+    // TODO: check if coin exists?
+    const coinName = req.path.replace(/\//, '').replace(/\.png/, '').toUpperCase();
+    const icon = text2png(coinName, { textColor: 'grey', font: '70px Futura' });
+    fs.writeFileSync('./public/cryptocurrency-icons/' + coinName + '.png', icon);
+    res.send(icon);
+});
 
 app.use("/login", new LoginRouter().router());
 app.use("/coin", new CoinRouter(coinService).router());
