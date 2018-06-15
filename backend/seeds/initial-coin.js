49f2e43f4950b5b8fd21a13b16d64ce7166f17d3
Didier Krux <didier@krux.co>
2018-06-15 12:18:34 +0800

    only insert new coins

--- backend/seeds/initial-coin.js
+++ backend/seeds/initial-coin.js
@@ -6,40 +6,51 @@ exports.seed = function (knex, Promise) {
         .then(function (response) {
             const coins = [];
             console.log(response.data);
-            return Promise.all(response.data.data.map(function (coin) {
-                const empty_source = {
-                    name: null,
-                    link: null
-                };
-                const insert_coin = {};
-                return knex('source')
-                    .insert([empty_source, empty_source, empty_source, empty_source, empty_source])
-                    .returning('id')
-                    .then(function (ids) {
-                        console.log(ids);
-                        if (ids && ids.length == 5) {
-                            insert_coin.official_website = ids[0];
-                            insert_coin.medium = ids[1];
-                            insert_coin.reddit = ids[2];
-                            insert_coin.twitter = ids[3];
-                            insert_coin.telegram = ids[4];
+            return knex
+                .select('coinmarketcap_id')
+                .from('coin')
+                .then(function (existing_ids) {
+                    const coinmarketcap_ids = existing_ids.map(coin => coin.coinmarketcap_id);
+                    console.log('coin ids in db:');
+                    console.log(coinmarketcap_ids);
+                    return Promise.all(response.data.data.map(function (coin) {
+                        if (coinmarketcap_ids.indexOf(coin.id) > -1) {    // coin already in DB
+                            return true;
+                        } else {    // new coin
+                            const empty_source = {
+                                name: null,
+                                link: null
+                            };
+                            const insert_coin = {};
+                            return knex('source')
+                                .insert([empty_source, empty_source, empty_source, empty_source, empty_source])
+                                .returning('id')
+                                .then(function (ids) {
+                                    if (ids && ids.length == 5) {
+                                        insert_coin.official_website = ids[0];
+                                        insert_coin.medium = ids[1];
+                                        insert_coin.reddit = ids[2];
+                                        insert_coin.twitter = ids[3];
+                                        insert_coin.telegram = ids[4];
+                                    }
+                                    insert_coin.coinmarketcap_id = coin.id;
+                                    insert_coin.name = coin.name;
+                                    insert_coin.symbol = coin.symbol;
+                                    // {
+                                    //     "id": 1, 
+                                    //     "name": "Bitcoin", 
+                                    //     "symbol": "BTC", 
+                                    //     "website_slug": "bitcoin"
+                                    // }
+                                    coins.push(insert_coin);
+                                });
                         }
-                        insert_coin.coinmarketcap_id = coin.id;
-                        insert_coin.name = coin.name;
-                        insert_coin.symbol = coin.symbol;
-                        // {
-                        //     "id": 1, 
-                        //     "name": "Bitcoin", 
-                        //     "symbol": "BTC", 
-                        //     "website_slug": "bitcoin"
-                        // }
-
-                        console.log(insert_coin);
-                        coins.push(insert_coin);
+                    })).then(function () {
+                        console.log('insert coins:');
+                        console.log(coins);
+                        return knex('coin').insert(coins);
                     });
-            })).then(function () {
-                return knex('coin').insert(coins);
-            })
+                });
         })
         .catch(function (error) {
             console.log(error);
