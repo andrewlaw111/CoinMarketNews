4ee88120a214af28407fc34c0b66f4774061f94f
Didier Krux <didier@krux.co>
2018-06-25 16:55:37 +0800

    price alert

---
+++ backend/price-alert.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+const PriceAlert = require("./src/cron/price-alert");
+
+PriceAlert();
---
+++ backend/src/cron/price-alert.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+// import { knex } from '../utils/init-app';
+
+var knex = require('knex')({
+    client: 'postgresql',
+    connection: {
+        host: 'localhost',
+        database: "postgres",
+        user: "postgres",
+        password: "password"
+    }
+});
+
+const ONESIGNAL_URI = "https://onesignal.com/api/v1/notifications";
+const ONESIGNAL_APP_ID = "155944be-3bde-4703-82f1-2545b31dc1ed";
+const ONESIGNAL_API_KEY = process.env.ONESIGNAL_API_KEY;
+
+const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Basic ${ONESIGNAL_API_KEY}`
+};
+
+const config = {
+    headers
+};
+
+module.exports = () => {
+    console.log('PriceAlert CRON start -------');
+    // upper alert
+    price_alert('>');
+    price_alert('<');
+    function price_alert(lower_or_higher) {
+        knex('price_alert')
+            .join('price', 'price_alert.price_id', 'price.id')
+            .join('currency', 'price.currency_id', 'currency.id')
+            .join('coin', 'price.coinmarketcap_id', 'coin.coinmarketcap_id')
+            .select('price_alert.id', 'coin.symbol as symbol', 'price_alert.price_point', 'price.price', 'currency.symbol as currency', 'currency.fiat')
+            .where('price_alert.active', '=', true)
+            .where('price_alert.upper', '=', (lower_or_higher === '>'))
+            .andWhere('price.price', lower_or_higher, knex.raw('price_alert.price_point'))
+            .then((alerts) => {
+                console.log(alerts);
+                for (const alert of alerts) {
+                    const price_point = (alert.fiat === true) ? parseInt(alert.price_point) : alert.price_point;
+                    const price = (alert.fiat === true) ? parseInt(alert.price) : alert.price;
+
+                    const notification_message = alert.symbol + ' price alert (' + lower_or_higher + price_point + ' ' + alert.currency + '): ' + price + ' ' + alert.currency;
+
+                    console.log(notification_message);
+
+                    var message = {
+                        app_id: ONESIGNAL_APP_ID,
+                        contents: { "en": notification_message },
+                        included_segments: ["All"]
+                    };
+
+                    axios.post(ONESIGNAL_URI, message, config)
+                        .then(function (response) {
+                            console.log('notification sent: ' + notification_message);
+                            // console.log(response);
+                            const price_alert_update = {};
+                            price_alert_update.active = false;
+                            knex('price_alert')
+                                .where({ id: alert.id })
+                                .update(price_alert_update).then((data) => {
+                                    if (data) {
+                                        console.log('alert ' + alert.id + ' desactivated');
+                                    }
+                                });
+                        })
+                        .catch(function (error) {
+                            console.log('error: ' + error);
+                        });
+                }
+            });
+    }
+}
