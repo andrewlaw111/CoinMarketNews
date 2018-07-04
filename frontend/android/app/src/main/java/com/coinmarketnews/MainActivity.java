ef5ed216cb054f8349943c2c3d32720ca6582ff2
Andrew Law <andrewlaw111@gmail.com>
2018-07-04 18:31:57 +0800

    added android splash

--- frontend/android/app/src/main/java/com/coinmarketnews/MainActivity.java
+++ frontend/android/app/src/main/java/com/coinmarketnews/MainActivity.java
@@ -13,8 +13,38 @@ package com.coinmarketnews;
 //         return "CoinMarketNews";
 //     }
 // }
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.util.TypedValue;
+
  import com.reactnativenavigation.controllers.SplashActivity;
 
  public class MainActivity extends SplashActivity {
+    @Override
+    public LinearLayout createSplashLayout() {
+        LinearLayout splash = new LinearLayout(this);
+        LinearLayout textBox = new LinearLayout(this);
+        TextView textView = new TextView(this);
+        Drawable launch_screen_bitmap = ContextCompat.getDrawable(getApplicationContext(),R.drawable.launch_screen_bitmap);
 
+        textView.setTextColor(Color.parseColor("#F8F8F8"));
+        textView.setText("CoinMarketNews");
+        textView.setGravity(Gravity.CENTER);
+        // textView.setLineHeight(30);
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 28);
+
+        textBox.addView(textView);
+
+        // textBox.setGravity(Gravity.CENTER_HORIZONTAL);        
+
+        splash.setGravity(Gravity.BOTTOM | Gravity.CENTER );
+        splash.setBackground(launch_screen_bitmap);
+        splash.addView(textBox);
+
+        return splash;
+    }
  }
