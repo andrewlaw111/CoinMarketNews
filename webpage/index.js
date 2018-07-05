708c5aad6b657f1d76e018af1c1ce98f6bf6467c
Andrew Law <andrewlaw111@gmail.com>
2018-07-05 15:05:11 +0800

    check if android or ios

--- webpage/index.js
+++ webpage/index.js
@@ -1,5 +1,10 @@
-var isRedirected = Response.redirected;
+const platform = window.navigator.platform
 
-if (isRedirected) {
-    $("#qr-code-modal").modal("show");
-}
+$("#download-button").click((e) => {
+    if (platform === "iPhone" || platform === "iPod" || platform === "iPad" || platform === "Android") {
+        return
+    } else {
+        e.preventDefault();
+        $("#qr-code-modal").modal("show");
+    }
+});
